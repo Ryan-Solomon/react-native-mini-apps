abea40dfc5d8838413b5aebc76243b159d4f16d9
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-09 18:19:11 -0600

    add text input

--- src/components/Cocktails/Cocktails.tsx
+++ src/components/Cocktails/Cocktails.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import { View } from 'react-native';
 
 const Cocktails = () => {
-  return <Text>Cocktails</Text>;
+  const [searchInput, setSearchInput] = useState('');
+
+  return (
+    <StyledContainer>
+      <StyledTextInput
+        onChangeText={(text: string) => setSearchInput(text)}
+        value={searchInput}
+        placeholder='Search Cocktails'
+      />
+    </StyledContainer>
+  );
 };
 
 export default Cocktails;
+
+const StyledContainer = styled.View`
+  align-items: center;
+`;
+
+const StyledTextInput = styled.TextInput`
+  margin-top: 1rem;
+  background: #272727;
+  width: 80vw;
+  padding: 0.5rem;
+  color: whitesmoke;
+  font-size: 1.6rem;
+`;
